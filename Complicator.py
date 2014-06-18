67bc53c2bf29368cf316f503ea0b8ea8e47f4ee9
NinaKos <nina.kos91@gmail.com>
2014-06-18 20:17:26 +0200

    Changes

--- Complicator.py
+++ Complicator.py
@@ -11,10 +11,10 @@ MAX_SCORE = 3
 # Funkcije lahko imajo argumente ali ne
 # Na podlagi tega kar dobijo vrzejo ven nek rezultat
 # Funkcijo definiraj na zacetku datoteke !!
-def starost(age):
-	if  age<20:
+def starost(stevilo):
+	if  stevilo<20:
 		return 1
-	elif age<30:
+	elif stevilo<30:
 		return 2
 	else:
 		return MAX_SCORE
@@ -27,13 +27,6 @@ def FBfriend(stevilo):
 	else:
 		return MAX_SCORE
 
-def calculate_complexity(p1, p2):
-	p1 = Decimal(p1)
-	p2 = Decimal(p2)
-	comp = p1+p2
-	realFeel = (p1+p2)/(2*MAX_SCORE)
-	return str(comp), str(realFeel)
-
 def bus(stevilo):
 	if stevilo<2:
 		return 1
@@ -42,85 +35,54 @@ def bus(stevilo):
 	else:
 		return MAX_SCORE
 
+def family(stevilo):
+	if stevilo<2:
+		return 1
+	elif stevilo<3:
+		return 2
+	else:
+		return MAX_SCORE
 
-# KOMENTARJI:
-# 1) Funkcija je vedno vracala 3, dokler smo imeli shranjeno naso
-#    starost kot niz znakov (string)
-# 2) raw_input() vrne string
-# 3) print starost(int(age))
-#    int - spremeni niz znakov v stevilo
-# 4) %d is for writing the numbers
+def money(stevilo):
+	if stevilo<100:
+		return 1
+	elif stevilo<300:
+		return 2
+	else:
+		return MAX_SCORE
 
-# PRIMERI funkcij:
-# def age_score():
-#	print starost(age)
-
-# def display_age(age):
-#	print age
-
-# def funny_fun():
-#	print age_score(5)
-
-# def simple_fun():
-#	print "Yeah!"
+def calculate_complexity(p1, p2, p3, p4, p5):
+	p1 = Decimal(p1)
+	p2 = Decimal(p2)
+	p3 = Decimal(p3)
+	p4 = Decimal(p4)
+	p5 = Decimal(p5)
+	comp = p1+p2+p3+p4+p5
+	realFeel = comp/(5*MAX_SCORE)
+	return str(comp), str(realFeel)
 
 
-# With raw input
 age = raw_input("How old are you? ")
-# print "I'm %r years old." % age
-
+age_score = starost(int(age))
 
 # Funkcija za stevilo prijateljev na facebook-u
-st = raw_input("Stevilo prijateljev na facebook-u? ")
+st = raw_input("Number of friends on facebook? ")
 fb_score = FBfriend(int(st))
 
-
-# Complexity
-score = calculate_complexity(starost(int(age)), fb_score)
-print "Koncni rezultat: ", score
-
-
+# Funkcija za stevilo avtobusov
 missBus = raw_input("How many buses did you miss? ")
 bus_score = bus(int(missBus))
-print bus_score
 
+# Funkcija za stevilo druzinskih clanov
+fam = raw_input("How many family members do you have? ")
+fam_score = family(int(fam))
 
-# deadline = raw_input("Do you have deadlines? ")
+# Funkcija za denar
+m = raw_input("How much money do you need? ")
+money_score = money(int(m))
 
-
-
-
-# print "Do you have deadlines?"
-# deadline = raw_input()
-# print "%r" % deadline
-
-
-
-
-# import datetime
-# currentTime = datetime.datetime.now()
-# Ce das import datetime lahko izbrises v ukazih datetime
-# today = date.today
-# print currentTime
-# Obvezno daj brez narekovajev, printa ti danasnji datum
-
-# print "Do you have a lack of money?"
-# money = raw_input()
-# print "%r" % money
-
-# print "How much?"
-# x = raw_input()
-# print "%r" % x
-
-# x = raw_input("How much?")
-# print x
-
-
-# attending = ["loving it", "hating it", "don't know"]
-# numberFamily = raw_input()
-# temp =
-# speedInternet =
-
-
-
+# Complexity
+score = calculate_complexity(age_score, fb_score, bus_score, 
+fam_score, money_score)
+print "Result: ", score
 
