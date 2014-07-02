f1c2404cda819768143b41984dc774c57a649bbc
NinaKos <nina.kos91@gmail.com>
2014-07-02 10:42:06 +0200

    Changes

--- Complicator.py
+++ Complicator.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from decimal import Decimal
+from sys import exit
+
 
 # Naslednja sprem je 'najpomembnejsa', zato z veliko
 MAX_SCORE = 3
@@ -45,17 +47,29 @@ def money(stevilo):
 	else:
 		return MAX_SCORE
 
-def calculate_complexity(p1, p2, p3, p4, p5):
+attending = ["loving it", "hating it", "don't know"]
+def school(izraz):
+	if attending=="loving it":
+		return 1
+	elif attending=="don't know":
+		return 2
+	else:
+		return MAX_SCORE
+
+
+def calculate_complexity(p1, p2, p3, p4, p5, p6):
 	p1 = Decimal(p1)
 	p2 = Decimal(p2)
 	p3 = Decimal(p3)
 	p4 = Decimal(p4)
 	p5 = Decimal(p5)
-	comp = p1+p2+p3+p4+p5
-	realFeel = comp/(5*MAX_SCORE)
-	return str(comp), str(realFeel)
+	p6 = Decimal(p6)
+	comp = p1+p2+p3+p4+p5+p6
+	realFeel = comp/(6*MAX_SCORE)
+	return float(comp), float(realFeel)
 
 
+# Funkcija za starost
 age = raw_input("How old are you? ")
 age_score = starost(int(age))
 
@@ -75,8 +89,12 @@ fam_score = family(int(fam))
 m = raw_input("How much money do you need? ")
 money_score = money(int(m))
 
+# Funkcija za solo
+s = raw_input("Do you like school? ")
+s_score = school(str(s))
+
 # Complexity
 score = calculate_complexity(age_score, fb_score, bus_score, 
-fam_score, money_score)
+fam_score, money_score, s_score)
 print "Result: ", score
 
